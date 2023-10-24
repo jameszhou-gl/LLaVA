e1ecb6bb88e1c9c86ee16a6eb5da5cf99590b6b8
jameszhou-gl <jameszhou.ustc@gmail.com>
2023-10-24 13:51:45 +0400

    run sqa in ciai

--- scripts/v1_5/eval/sqa.sh
+++ scripts/v1_5/eval/sqa.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+#SBATCH --job-name=exp
+#SBATCH --output=output.%A.txt           # Standard output and error log
+#SBATCH --nodes=1                                     # Run all processes on a single node
+#SBATCH --ntasks=1                                    # Run on a single CPU
+#SBATCH --mem=80G                                 # Total RAM to be used
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=64                     # Number of CPU cores
+#SBATCH -p long                                          # Use the gpu partition
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate llava
 
 python -m llava.eval.model_vqa_science \
     --model-path liuhaotian/llava-v1.5-13b \
-    --question-file ./playground/data/eval/scienceqa/llava_test_CQM-A.json \
-    --image-folder ./playground/data/eval/scienceqa/images/test \
-    --answers-file ./playground/data/eval/scienceqa/answers/llava-v1.5-13b.jsonl \
+    --question-file /l/users/guanglin.zhou/llava/playground/data/eval/scienceqa/llava_minitest_CQM-A.json \
+    --image-folder /l/users/guanglin.zhou/llava/playground/data/eval/scienceqa/images/test \
+    --answers-file /l/users/guanglin.zhou/llava/playground/data/eval/scienceqa/answers/llava-v1.5-13b.jsonl \
     --single-pred-prompt \
     --temperature 0 \
     --conv-mode vicuna_v1
 
 python llava/eval/eval_science_qa.py \
-    --base-dir ./playground/data/eval/scienceqa \
-    --result-file ./playground/data/eval/scienceqa/answers/llava-v1.5-13b.jsonl \
-    --output-file ./playground/data/eval/scienceqa/answers/llava-v1.5-13b_output.jsonl \
-    --output-result ./playground/data/eval/scienceqa/answers/llava-v1.5-13b_result.json
+    --base-dir /l/users/guanglin.zhou/llava/playground/data/eval/scienceqa \
+    --result-file /l/users/guanglin.zhou/llava/playground/data/eval/scienceqa/answers/llava-v1.5-13b.jsonl \
+    --output-file /l/users/guanglin.zhou/llava/playground/data/eval/scienceqa/answers/llava-v1.5-13b_output.jsonl \
+    --output-result /l/users/guanglin.zhou/llava/playground/data/eval/scienceqa/answers/llava-v1.5-13b_result.json
